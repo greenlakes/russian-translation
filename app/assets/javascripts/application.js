74cd703e52d24a0d1f70be162d6a6bfca308339d
greenlakes <greenlakes2005@gmail.com>
2017-02-01 21:34:35 +0000

    Metro template added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 // supported directives.
 //
 //= require jquery
-//= require jquery-ui/effects/effect-blind
+//= require bootstrap-sprockets
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
